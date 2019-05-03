7a441d0aa85268afd3a858aac32dee52fad7b263
Estelle Olawinski <estelleolawinski@mbpdeestelle2.home>
2019-05-03 13:24:58 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'mon-premier';
+  title = "WHY I LOVE MODJO";
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+
+  appareils = [
+    {
+      name: "Assis",
+      status: "oui"
+    },
+    {
+      name: "Roule",
+      status: "oui"
+    },
+    {
+      name: "Fais le mort",
+      status: "non"
+    },
+    {
+      name: "Couché",
+      status: "oui"
+    },
+    {
+      name: "La patte",
+      status: "oui"
+    }
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+  onAllumer() {
+    console.log("Il maitrise touuuuuut!");
+  }
 }
