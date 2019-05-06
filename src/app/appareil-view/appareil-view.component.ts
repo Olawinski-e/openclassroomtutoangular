6afc9437e145821b773995686c496212b9eca8de
Estelle Olawinski <estelleolawinski@mbpdeestelle2.home>
2019-05-06 14:53:32 +0200

    forms template

--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { AppareilService } from "../services/appareil.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-appareil-view",
@@ -7,8 +8,8 @@ import { AppareilService } from "../services/appareil.service";
   styleUrls: ["./appareil-view.component.scss"]
 })
 export class AppareilViewComponent implements OnInit {
-  isAuth = false;
   appareils: any[];
+  appareilSubscription: Subscription;
 
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
@@ -17,14 +18,15 @@ export class AppareilViewComponent implements OnInit {
     }, 2000);
   });
 
-  constructor(private appareilService: AppareilService) {
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 1000);
-  }
+  constructor(private appareilService: AppareilService) {}
 
   ngOnInit() {
-    this.appareils = this.appareilService.appareils;
+    this.appareilSubscription = this.appareilService.appareilsSubject.subscribe(
+      (appareils: any[]) => {
+        this.appareils = appareils;
+      }
+    );
+    this.appareilService.emitAppareilSubject();
   }
 
   onAllumer() {
@@ -40,5 +42,9 @@ export class AppareilViewComponent implements OnInit {
     } else {
       return null;
     }
+  }
+
+  ngOnDestroy() {
+    this.appareilSubscription.unsubscribe();
   }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
 import { tick } from "@angular/core/src/render3";
+import { Subject } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class AppareilService {
+  appareilsSubject = new Subject<any[]>();
   appareils = [
     {
       id: 1,
@@ -33,25 +35,33 @@ export class AppareilService {
     }
   ];
 
+  emitAppareilSubject() {
+    this.appareilsSubject.next(this.appareils.slice());
+  }
+
   constructor() {}
 
   switchOnAll() {
     for (let appareil of this.appareils) {
       appareil.status = "oui";
     }
+    this.emitAppareilSubject();
   }
   swithOffAll() {
     for (let appareil of this.appareils) {
       appareil.status = "non";
+      this.emitAppareilSubject();
     }
   }
 
   switchOnOne(i: number) {
     this.appareils[i].status = "oui";
+    this.emitAppareilSubject();
   }
 
   switchOffOne(i: number) {
     this.appareils[i].status = "non";
+    this.emitAppareilSubject();
   }
 
   getAppareilById(id: number) {
@@ -59,5 +69,18 @@ export class AppareilService {
       return s.id === id;
     });
     return appareil;
+  }
+
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: "",
+      status: ""
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[this.appareils.length - 1].id + 1;
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
   }
 }
